b7082faca1b2b31d56bf498fa32b607b56a2c66f
ning-g-m <3436464181@qq.com>
2025-08-14 12:50:45 +0800

    Fix resource paths for GitHub Pages deployment

--- vite-vue-mcisee/src/scripts/module/theme.js
+++ vite-vue-mcisee/src/scripts/module/theme.js
@@ -10,18 +10,18 @@ const themeChanged = (() => {
     localStorage.setItem('theme', value);
     switch (value) {
         case 'light':
-            $('#theme').attr('href', 'assets/light.css');
+            $('#theme').attr('href', './assets/light.css');
             if($('style#classic').length) $('style').remove('#classic');
             $('[alt="Stars Over Time"]').attr('src', 'https://starchart.cc/teaSummer/MCiSEE.svg?background=%2300000000&axis=%23101010&line=%236b63ff');
             break;
         case 'dark':
-            $('#theme').attr('href', 'assets/dark.css');
+            $('#theme').attr('href', './assets/dark.css');
             if($('style#classic').length) $('style').remove('#classic');
             $('[alt="Stars Over Time"]').attr('src', 'https://starchart.cc/teaSummer/MCiSEE.svg?background=%2300000000&axis=%23ffffff&line=%236b63ff');
             break;
         case 'system':
             if (window.matchMedia('(prefers-color-scheme: dark)').matches) system = 'dark';
-            $('#theme').attr('href', `assets/${system}.css`);
+            $('#theme').attr('href', `./assets/${system}.css`);
             if($('style#classic').length) $('style').remove('#classic');
             $('[alt="Stars Over Time"]').attr('src', 'https://starchart.cc/teaSummer/MCiSEE.svg?background=%2300000000&axis=%23101010&line=%236b63ff');
             if (system == 'dark') {
@@ -31,12 +31,12 @@ const themeChanged = (() => {
         case 'classic':
             const classic = `<style id="classic">
     div.background {
-        background-image: url('assets/image/classic.webp');
+        background-image: url('./assets/image/classic.webp');
         transform: none;
         filter: none;
     }
 </style>`;
-            $('#theme').attr('href', 'assets/light.css');
+            $('#theme').attr('href', './assets/light.css');
             // document.adoptedStyleSheets.push(classic);
             // Object.isExtensible(document.adoptedStyleSheets) === false
             // on some browsers
@@ -59,7 +59,7 @@ $(window.matchMedia('(prefers-color-scheme: dark)')).change((event) => {
 const device = browser.parse();
 if (device.device == 'Mobile') {
     $.ajax({
-        url: 'assets/mobile.css',
+        url: './assets/mobile.css',
         success: (data) => $('head').append(`<style id="mobile">${data}</style>`)
     });
 }
